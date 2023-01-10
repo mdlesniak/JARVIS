33255776f9d0288b99b8caf86f122384cd09b6a5
mlesniak <mdlesniak@gmail.com>
2023-01-10 10:22:52 -0500

    OpenAI org and key

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
+import openai as ai
 
-# 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
-
+# ai.organization = 'org-fiAKNGE2G2ivhR2h2M9tUQMF'
+# ai.api.key = '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'
+ai.organization = os.environ.get('OPENAI_API_KEY')
+ai.api_key = os.environ.get('OPENAI_ORGANIZATION')
 
 if __name__ == '__main__':
     print('It works!')
